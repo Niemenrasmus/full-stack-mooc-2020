4edb1b63940737c22660078e456ecdf521e37c98
Niemenrasmus <niemenrasmus@gmail.com>
2021-03-05 08:55:18 +0200

    dev

---
+++ full-stack-mooc/full-stack-mooc-2020-part3/precache-manifest.d75733832275bd57f99a41675c888c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed833f7d8fe681ae930ae8d05ff74c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01706674e6945aaf6a63",
+    "url": "/static/css/main.99f95a05.chunk.css"
+  },
+  {
+    "revision": "7e2e84670a0616be5fb0",
+    "url": "/static/js/2.92cad084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.92cad084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01706674e6945aaf6a63",
+    "url": "/static/js/main.436bd4fc.chunk.js"
+  },
+  {
+    "revision": "f642d533f409b3f3a92f",
+    "url": "/static/js/runtime-main.4ff6ba4f.js"
+  }
+]);
